45cc67a962ec68f7ac7769424d5e56c5ba936c34
MARCELOSALVATIERRA926 <MARCELOSALVATIERRA926@GMAIL.COM>
2025-08-11 22:00:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,3 +27,17 @@ ZGFzU1NIL3Jhdy9tYWluL211bHQvbWVnYWhib3QuemlwIC1PIC9ldGMvbWVnYWhib3QuemlwID4g
 L2Rldi9udWxsIDI+JjE7IHVuemlwIC9ldGMvbWVnYWhib3QuemlwOyBtdiBtZWdhaGJvdCAvZXRj
 OyBlY2hvOyBlY2hvICJJbnN0YWxhw6fDo28gdGVybWluYWRhISBOw6NvIGVzcXVlw6dhIGRlIGVk
 aXRhciBzZXVzIGRhZG9zIG5vIGFycXVpdm8gY29uZmlnLmpzIjsgZWNobw==' | base64 -d )
+
+
+#!/bin/bash
+[[ "$(whoami)" != "root" ]] && {
+    echo; echo "Instale com usuario root!"; echo; exit 0
+}
+[[ -e /etc/megahbot/index.js ]] && {
+echho; echo "@tualizando bot, aguarde..."; echo; wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/index.js > /dev/null 2>&1; wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/veri.js > /dev/null 2>&1; wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/gerar.js > /dev/null 2>&1; wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/qrcode > /dev/null 2>&1; chmod +x qrcode; mv qrcode /bin; mv index.js /etc/megahbot; mv veri.js gerar.js /etc/megahbot/src; echo; echo "Atualizaçço terminada! digite onbot para rodar o novo programa"; echo; exit 0 }
+
+apt update -y; echo; echo "Instalando bot, aguarde..."; echo; apt install nodejs -y > /dev/null 2>&1; apt install unzip -y > /dev/null 2>&1; apt install screen -y > /dev/null 2>&1; apt install wget -y > /dev/null 2>&1; wget https://raw.githubusercontent.com/MARCELOSALVATIERRA926/boot001/refs/heads/main/mult/onbot > /dev/null 2>&1; wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/offbot > /dev/null 2>&1; wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/qrcode > /dev/null 2>&1; wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/config.js > /dev/null 2>&1; chmod +x onbot offbot qrcode; mv onbot offbot qrcode /bin; wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/megahbot.zip -O /etc/megahbot.zip > /dev/null 2>&1; unzip /etc/megahbot.zip;
+mv megahbot /etc; echo; echo "Instalaão terminada! Não esqueça de editar seus dados no
+arquivo config.js"; echo[root@vps-5218676-x ~] #
+
+
