508678ae115478fb1ed7ef5d9e27e83e395c6499
MARCELOSALVATIERRA926 <MARCELOSALVATIERRA926@GMAIL.COM>
2025-08-14 13:08:28 -0300

    Update config.js

--- mult/config.js
+++ mult/config.js
@@ -1,7 +1,7 @@
 config = {
-  dono:"559184035474", //tu numero con 55+ddd sin el 9
+  dono:"5493814568136", //tu numero con 55+ddd sin el 9
   tempo_teste:"3", //tiempo de prueba en horas
-  valorLogin: 0.10, //Valor de inicio de sesión SSH
+  valorLogin: 5000.0, //Valor de inicio de sesión SSH
   nomeLoja:"Loja teste", //nombre de su tienda online
   linkApp:"LinkAqui", //Enlace a tu aplicación, PlayStore/Mediafire
   token_mp:"TokenAqui" //tu token de Mercado Pago
