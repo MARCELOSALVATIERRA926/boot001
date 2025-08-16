991d284fa930307b3e838cad3db47f96f87cce95
MARCELOSALVATIERRA926 <MARCELOSALVATIERRA926@GMAIL.COM>
2025-08-16 14:08:33 -0300

    Update config.js

--- mult/config.js
+++ mult/config.js
@@ -1,10 +1,10 @@
 config = {
   dono:"5493814568136", //tu numero con 55+ddd sin el 9
   tempo_teste:"3", //tiempo de prueba en horas
-  valorLogin: 5000.0, //Valor de inicio de sesión SSH
-  nomeLoja:"Loja teste", //nombre de su tienda online
-  linkApp:"LinkAqui", //Enlace a tu aplicación, PlayStore/Mediafire
-  token_mp:"TokenAqui" //tu token de Mercado Pago
+  valorLogin: 6000.0, //Valor de inicio de sesión SSH
+  nomeLoja:"FenixVPs912", //nombre de su tienda online
+  linkApp:"https://play.google.com/store/apps/details?id=xyz.easypro.httpcustom", //Enlace a tu aplicación, PlayStore/Mediafire
+  token_mp:"https://mpago.li/2XhZwGZ" //tu token de Mercado Pago
 }
 module.exports = {
   config
